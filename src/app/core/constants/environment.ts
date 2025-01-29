af01dd8b5eb07739f15cb8847038cc72abbbb0f0
Fernando Cano <fernandocanotapias@gmail.com>
2025-01-29 13:38:01 -0500

    Cambio de ruta de backend

--- src/app/core/constants/environment.ts
+++ src/app/core/constants/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://wxl1fwsm-3000.use2.devtunnels.ms/api/v2', //'http://localhost:3000/api/v2',
+  apiUrl: 'https://api.zizicar.com/api/v2', //'http://localhost:3000/api/v2',
   featureFlag: true,
 };
